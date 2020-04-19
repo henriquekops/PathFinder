1f87356612e920fffb634c792b872f09042afaeb
henriquekops <henriquekops@gmail.com>
2020-04-19 19:36:00 -0300

    initialize genetic algorithm's class and test

---
+++ path-finder/src/main/java/app/GeneticAlgorithm.java
@@ -0,0 +1,15 @@
+package app;
+
+/***
+ * Genetic algorithm class
+ */
+public class GeneticAlgorithm {
+
+    /**
+     * GeneticAlgorithm class constructor
+     */
+    public GeneticAlgorithm() {
+        print("Not implemented yet :(")
+    }
+
+}
---
+++ path-finder/src/test/java/app/GeneticAlgorithmTest.java
@@ -0,0 +1,20 @@
+package app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for GeneticAlgorithm class.
+ */
+public class GeneticAlgorithmTest {
+
+    /**
+     * Empty assert
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
