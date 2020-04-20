65d886eb0cc21f7bfefff152fda0f96387de86b0
henriquekops <henriquekops@gmail.com>
2020-04-19 21:00:58 -0300

    create boilerplate for genetic algorithm's class and tests

---
+++ path-finder/src/main/java/algorithms/GeneticAlgorithm.java
@@ -0,0 +1,82 @@
+package algorithms;
+
+/***
+ * Genetic algorithm class
+ */
+public class GeneticAlgorithm {
+
+    /*
+    NOTES:
+      Expected input:
+        $ java main -p {--path} ~/UserX/... -g {--generations} 100 -s {--solutions} 5 [--debug]
+      Agent movements:
+        N=up, S=down, E=right, W=left
+        NE=diagonal-up-right, NW=diagonal-up-left, SE=diagonal-down-right, SW=diagonal-down-left
+     */
+
+    /**
+     * GeneticAlgorithm class constructor
+     */
+    public GeneticAlgorithm() {
+        System.out.println("Not implemented yet :(");
+    }
+
+    /**
+     * Applies the genetic algorithm for maze's path finding
+     * @param maze Maze that contains a path to be found
+     * @return Same maze from input but containing its solution
+     */
+    public String[][] findPath(String[][] maze) {
+        System.out.println("Not implemented yet :(");
+        return maze;
+    }
+
+    /**
+     * Applies the heuristic function for generate fit score
+     * @param agent Agent's identifier to recover its maze's solution
+     * @return Score given to parametrized agent
+     */
+    public int heuristicFunction(int agent) {
+        System.out.println("Not implemented yet :(");
+        return 0;
+    }
+
+    /**
+     * Selects an agent through elitism algorithm (best of all fits)
+     * @param population Population to extract best agent's genetic load
+     * @return Best agent's solution to carry to next generation
+     */
+    public int elitismAlgorithm(String[][] population) {
+        System.out.println("Not implemented yet :(");
+        return 0;
+    }
+
+    /**
+     * Selects an agent through tournament algorithm (best between two randomized fits)
+     * @param everyAgentScores Every agent's scores to apply the algorithm
+     * @return Best fit agent
+     */
+    public int tournamentAlgorithm(int[] everyAgentScores) {
+        System.out.println("Not implemented yet :(");
+        return 0;
+    }
+
+    /**
+     * Crosses genetic load between two agents using single point technique
+     *
+     * @param population Current agent population
+     * @return Intermediate agent population generated
+     */
+    public String[][] crossoverAlgorithm(String[][] population) {
+        System.out.println("Not implemented yet");
+        return population;
+    }
+
+    /**
+     * Shows current population
+     */
+    public void showPopulation() {
+        System.out.println("Not implemented yet :(");
+    }
+
+}
---
+++ path-finder/src/test/java/algorithms/GeneticAlgorithmTest.java
@@ -0,0 +1,65 @@
+package algorithms;
+
+// built-in dependencies
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * Unit test for GeneticAlgorithm class.
+ */
+public class GeneticAlgorithmTest {
+
+    /**
+     * Find path method's test
+     */
+    @Test
+    public void testFindPath()
+    {
+        String[][] maze = new String[10][10];
+        GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm();
+        assertEquals(geneticAlgorithm.findPath(maze), maze);
+    }
+
+    /**
+     * Heuristic function method's test
+     */
+    @Test
+    public void testHeuristicFunction()
+    {
+        GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm();
+        assertEquals(geneticAlgorithm.heuristicFunction(0), 0);
+    }
+
+    /**
+     * Elitism algorithm method's test
+     */
+    @Test
+    public void testElitismAlgorithm()
+    {
+        String[][] population = new String[10][10];
+        GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm();
+        assertEquals(geneticAlgorithm.elitismAlgorithm(population), 0);
+    }
+
+    /**
+     * Tournament algorithm method's test
+     */
+    @Test
+    public void testTournamentAlgorithm()
+    {
+        int[] everyAgentScore = new int[10];
+        GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm();
+        assertEquals(geneticAlgorithm.tournamentAlgorithm(everyAgentScore), 0);
+    }
+
+    /**
+     * Crossover algorithm method's test
+     */
+    @Test
+    public void testCrossoverAlgorithm()
+    {
+        String[][] population = new String[10][10];
+        GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm();
+        assertEquals(geneticAlgorithm.crossoverAlgorithm(population), population);
+    }
+}
